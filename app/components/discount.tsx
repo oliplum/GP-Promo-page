c29b857046de484164d2e5a9b5398bdc90e8ad4e
oliplum <oli@globalpodium.com>
2026-01-16 01:20:58 +1300

    tweaks

--- app/components/discount.tsx
+++ app/components/discount.tsx
@@ -23,7 +23,7 @@ export default function Discount({ selectedEvents, allEvents, onSelectAll, onDes
     return (
         <div className="discount-banner">
             <div className="discount-text">
-                <h2 className="text-xl font-bold mb-3">Discounts for the winter sale</h2>
+                <h2 className="text-xl font-bold mb-3">Discounts for the New Year Sale</h2>
                 <ul className="text-xs list-disc list-inside space-y-1 inline-block text-left">
                     <li>All workshops 50% off</li>
                     <li>Buy 10 or more for an additional 10% off (total 60%)</li>
